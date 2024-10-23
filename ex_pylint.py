cb60d395056bc188f1b5451af756d6b345d56f3d
Victor Marquardt <nologistic@gmail.com>
2024-10-23 08:22:32 -0300

    config: Adding pylint to the project

---
+++ ex_pylint.py
@@ -0,0 +1,10 @@
+
+print("Olá mundo")
+
+def minha_funcao():
+    ''' Serve para printar teste'''
+    print('teste')
+
+# snake_case -> Variáveis e funções em Python -> minha_funcao()
+# cammelCase
+# PascalCase -> Classes em Python
