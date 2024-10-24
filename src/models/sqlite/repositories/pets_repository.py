f5c94e63c53d72d16d2f0875ae378d62d3577327
Victor Marquardt <nologistic@gmail.com>
2024-10-24 16:37:08 -0300

    test: finishing tests in pets repository

---
+++ src/models/sqlite/repositories/pets_repository.py
@@ -0,0 +1,32 @@
+from typing import List
+from sqlalchemy.orm.exc import NoResultFound
+from src.models.sqlite.entities.pets import PetsTable
+
+class PetsRepository:
+    def __init__(self, db_connection) -> None:
+        self.__dbconnection = db_connection
+
+    def list_pets(self) -> List[PetsTable]:
+        with self.__dbconnection as database:
+            try:
+                pets = (database
+                        .session
+                        .query(PetsTable)
+                        .all())
+                return pets
+            except NoResultFound:
+                return []
+            
+    def delete_pets(self, name: str) -> None:
+        with self.__dbconnection as database:
+            try:
+                (
+                database.session
+                .query(PetsTable)
+                .filter(PetsTable.name == name)
+                .delete()
+                )
+                database.session.commit() # Fazer um commit no banco de dados. FUNCIONA PARA ALTERAÇÕES DE DADOS (CUD)
+            except Exception as exception:
+                database.session.rollback() # Se algo der errado, volte o banco para o mesmo estado que ele estava antes
+                raise exception
