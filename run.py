271765cddcfc71bea4603eebc7e850bbe932dc8b
Victor Marquardt <nologistic@gmail.com>
2024-10-28 14:24:07 -0300

    adding DB

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from src.main.server.server import app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True) #  Debug=False para ambiemnte de prod
+    app.run(host="0.0.0.0", port=3000) #  Debug=False para ambiemnte de prod
